acdf6f14d62b0f61eb76e1884f5e82d8e2298e15
JunhoCho <junhocho@snu.ac.kr>
2017-07-13 01:37:50 +0900

    prem_ticker now updates not refresh

--- premium_ticker.py
+++ premium_ticker.py
@@ -92,7 +92,6 @@ Arbys = [Arby(p,k) for p,k in zip(polo_bots, krx_bots)]
 
 ### START arbitrage!!
 
-iter_ticker = 0
 
 update_period = 5 # seconds
 def wait(iter_s):
@@ -117,48 +116,73 @@ Ys_polo_pirce = [np.zeros(0),np.zeros(0),np.zeros(0),np.zeros(0),np.zeros(0)]
 Ys_krx_price= [np.zeros(0),np.zeros(0),np.zeros(0),np.zeros(0),np.zeros(0)]
 Ys_prems2show = [1,2,3,4,5]
 Ys_price2show = [6,7,8,9,0]
+polo_price = [1,2,3,4,5]
+krx_price = [1,2,3,4,5]
 
-X = np.column_stack((np.arange(0), np.arange(0)))
 
+iter_ticker = 0
+time_stamp = 0
 while(True):
     tic = time.time()
 
+    # TODO Better loop. prevent one fail --> all fail
     try:
         for k,p in zip(krx_bots, polo_bots):
             k.collect_price()
             p.collect_price()
     except Exception as e:
         logger.exception("waiting next iter")
-        continue
     else:
-        for _, arby in enumerate(Arbys):
-            premiums[_] = arby.ticker_premium(threshold)
+        for i, arby in enumerate(Arbys):
+            premiums[i] = arby.ticker_premium(threshold)
+            polo_price[i] = (polo_bots[i].sell_price + polo_bots[i].buy_price)/2
+            krx_price[i] = (krx_bots[i].sell_price + krx_bots[i].buy_price)/2
         for i in range(5):
             Ys_pos[i] = np.append(Ys_pos[i], premiums[i][0])
             Ys_neg[i] = np.append(Ys_neg[i], premiums[i][1])
-            Ys_polo_pirce[i] = np.append(Ys_polo_pirce[i], (polo_bots[i].sell_price + polo_bots[i].buy_price)/2)
-            Ys_krx_price[i] = np.append(Ys_krx_price[i], (krx_bots[i].sell_price + krx_bots[i].buy_price)/2)
+            Ys_polo_pirce[i] = np.append(Ys_polo_pirce[i], polo_price[i])
+            Ys_krx_price[i] = np.append(Ys_krx_price[i], krx_price[i])
 
             Ys_prems2show[i] = np.column_stack((Ys_pos[i], Ys_neg[i]))
             Ys_price2show[i] = np.column_stack((Ys_polo_pirce[i], Ys_krx_price[i]))
 
-        for i in range(5):
-            wins_prem_monitor[i] = viz.line(
-                Y = Ys_prems2show[i],
-                win = wins_prem_monitor[i],
-                opts =dict(
-                    title = alt_list[i],
-                    legend = ['+', '-']
+        X = np.column_stack((np.array(time_stamp), np.array(time_stamp)))
+        # TODO : global timestamp how to append
+        if iter_ticker ==  0:
+            for i in range(5):
+                wins_prem_monitor[i] = viz.line(
+                    X = X,
+                    Y = Ys_prems2show[i],
+                    win = wins_prem_monitor[i],
+                    opts =dict(
+                        title = alt_list[i],
+                        legend = ['+', '-']
+                    )
                 )
-            )
-        for i in range(5):
-            wins_price_ticker[i] = viz.line(
-                Y = Ys_price2show[i],
-                win = wins_price_ticker[i],
-                opts =dict(
-                    title = alt_list[i]+' sell price',
-                    legend = ['polo', 'bith'])
-            )
+            for i in range(5):
+                wins_price_ticker[i] = viz.line(
+                    X = X,
+                    Y = Ys_price2show[i],
+                    win = wins_price_ticker[i],
+                    opts =dict(
+                        title = alt_list[i]+' sell price',
+                        legend = ['polo', 'bith'])
+                )
+        else:
+            for i in range(5):
+                Y = np.column_stack((premiums[i][0], premiums[i][1]))
+                viz.updateTrace(
+                    X = X,
+                    Y = Y,
+                    win = wins_prem_monitor[i],
+                )
+            for i in range(5):
+                Y = np.column_stack((polo_price[i], krx_price[i]))
+                viz.updateTrace(
+                    X = X,
+                    Y = Y,
+                    win = wins_price_ticker[i],
+                )
 
     # print('\t\t{}\t\t|\tPOLO\t\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t|\t{}\t'
     #         .format('BITHUMB', 'ETH','ETC','LTC','DASH','XRP'))
@@ -169,4 +193,5 @@ while(True):
     wait(tic)
     toc = time.time()
     print(toc-tic, datetime.now())
+    time_stamp += toc-tic
     iter_ticker += 1
