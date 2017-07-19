ea087e05aca04c6ed4e71c425486e8de1abf1fa9
JunhoCho <junhocho@snu.ac.kr>
2017-07-18 23:23:00 -1000

    simulation mode implementing.

--- plot.py
+++ plot.py
@@ -37,5 +37,39 @@ win = viz.line(
 
 
 import pandas as pd
-df = pd.DataFrame(np.random.rand(10))
-df.to_csv("file_path.csv")
+# df = pd.DataFrame(np.random.rand(10))
+# df.to_csv("file_path.csv")
+
+alt_name = 'ETH'
+
+X = pd.read_csv('./log/'+alt_name+'/X.csv')
+Y_prem_pos = pd.read_csv("./log/"+alt_name+"/Y_prem_pos.csv")
+Y_prem_neg = pd.read_csv("./log/"+alt_name+"/Y_prem_neg.csv")
+Y_krx_altkrw_sell_price = pd.read_csv("./log/"+alt_name+"/Y_krx_altkrw_sell_price.csv")
+Y_krx_altkrw_buy_price = pd.read_csv("./log/"+alt_name+"/Y_krx_altkrw_buy_price.csv")
+Y_krx_btckrw_sell_price = pd.read_csv("./log/"+alt_name+"/Y_krx_btckrw_sell_price.csv")
+Y_krx_btckrw_buy_price = pd.read_csv("./log/"+alt_name+"/Y_krx_btckrw_buy_price.csv")
+Y_krx_sell_price = pd.read_csv("./log/"+alt_name+"/Y_krx_sell_price.csv")
+Y_krx_buy_price = pd.read_csv("./log/"+alt_name+"/Y_krx_buy_price.csv")
+Y_polo_sell_price = pd.read_csv("./log/"+alt_name+"/Y_polo_sell_price.csv")
+Y_polo_buy_price = pd.read_csv("./log/"+alt_name+"/Y_polo_buy_price.csv")
+Y_polo_btcusd_price = pd.read_csv("./log/"+alt_name+"/Y_polo_btcusd_price.csv")
+
+# TODO : inconsistent length..?
+X = np.array(X)[:,1]
+Y_prem_pos = np.array(Y_prem_pos)[:,1]
+Y_prem_neg = np.array(Y_prem_neg)[:,1]
+Y_krx_altkrw_sell_price = np.array(Y_krx_altkrw_sell_price)[:,1]
+Y_krx_altkrw_buy_price = np.array(Y_krx_altkrw_buy_price)[:,1]
+Y_krx_btckrw_sell_price = np.array(Y_krx_btckrw_sell_price)[:,1]
+Y_krx_btckrw_buy_price = np.array(Y_krx_btckrw_buy_price)[:,1]
+Y_krx_sell_price = np.array(Y_krx_sell_price)[:,1]
+Y_krx_buy_price = np.array(Y_krx_buy_price)[:,1]
+Y_polo_sell_price = np.array(Y_polo_sell_price)[:,1]
+Y_polo_buy_price = np.array(Y_polo_buy_price)[:,1]
+Y_polo_btcusd_price = np.array(Y_polo_btcusd_price)[:,1]
+
+win = viz.line(
+        X=X,
+        Y=Y_polo_btcusd_price
+        )
